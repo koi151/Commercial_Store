88daf82c47ec48a1f236b95b87e348229e12d651
Khôi Nguyễn <originalkhoi151@gmail.com>
2023-09-01 12:44:14 +0700

    update nav scrolling effect

--- src/components/Layout/Header/header.js
+++ src/components/Layout/Header/header.js
@@ -4,6 +4,22 @@ import ShortNotes from './ShortNotes/shortNotes';
 import './header.scss'
 
 function Header() {
+
+  const navbar = document.querySelector('header');
+  let lastScrollTop = 0;
+
+  window.addEventListener('scroll', () => {
+    const scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop && scrollTop > 90 ) 
+      // Scrolling down, hide the navbar
+      navbar.style.top = '-110px';
+    else 
+      // Scrolling up, show the navbar
+      navbar.style.top = '0';
+    lastScrollTop = scrollTop;
+  });
+
   return (
     <>
       <header>
